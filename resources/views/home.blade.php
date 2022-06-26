95c023660cf73906909d6e235355664216275034
Leyton Nightingale <leyton.nightingale.online@gmail.com>
2022-06-26 14:32:10 +0200

    new update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,20 +45,25 @@
             <div class="parallax__layer parallax__layer__0" style="height: 100vh; width:100vw; margin-left:0; left:0; background-image:url(https://i.pinimg.com/originals/c8/4c/0a/c84c0aeedf27fb4191b427f1bd9d9f3c.jpg); background-size:cover;">
 
             </div>
+
+            <div class="parallax__layer parallax__layer__7" style="height: 100vh; width:100vw;display: flex;align-items: flex-end;justify-content: center;z-index: 2;">
+               <i class="material-icons-round" style="font-size:100px">keyboard_double_arrow_down</i>
+            </div>
+
             <div class="parallax__layer parallax__layer__6">
                 <canvas id="canvas" style="filter:brightness(1.1);"></canvas>
             </div>
             <div class="parallax__layer parallax__layer__2">
-                <img src="{{ asset('images/hi_r/io.png') }}" style="margin-left:53vw; height: 10vw; bottom: 12vh; filter: brightness(0.7); z-index:0;"/>
+                <img src="{{ asset('images/hi_r/io.png') }}" style="margin-left:53vw; height: 3vw; bottom: 12vh; filter: brightness(0.7); z-index:0;"/>
             </div>
             <div class="parallax__layer parallax__layer__3">
-                <img src="{{ asset('images/hi_r/europa.png') }}" style="margin-left:61vw; height: 6vw; bottom: 19vh; filter:brightness(0.9); z-index:1;"/>
+                <img src="{{ asset('images/hi_r/europa.png') }}" style="margin-left:61vw; height: 2vw; bottom: 19vh; filter:brightness(0.9); z-index:1;"/>
             </div>
             <div class="parallax__layer parallax__layer__4">
-                <img src="{{ asset('images/hi_r/ganymede.png') }}" style="margin-left:68vw; height: 15vw; bottom: 27vh; filter:brightness(1.1); z-index:2;"/>
+                <img src="{{ asset('images/hi_r/ganymede.png') }}" style="margin-left:68vw; height: 3vw; bottom: 27vh; filter:brightness(1.1); z-index:2;"/>
             </div>
             <div class="parallax__layer parallax__layer__5">
-                <img src="{{ asset('images/hi_r/callisto.png') }}" style="margin-left:80vw; height: 22vw; bottom: 57vh; filter:brightness(1.3); z-index:3;"/>
+                <img src="{{ asset('images/hi_r/callisto.png') }}" style="margin-left:80vw; height: 4vw; bottom: 57vh; filter:brightness(1.3); z-index:3;"/>
             </div>
             <div class="parallax__layer parallax__layer__0">
                 <img src="{{ asset('images/hi_r/jupiter.png') }}" style="margin-left:-10vw; height: 65vw; bottom: -30vh; filter:brightness(1.3); z-index:4;"/>
@@ -67,12 +72,15 @@
 
                 <div style="display: flex; flex-direction:column; justify-content: center; align-items: center; z-index:10; text-align:center;">
                     </br></br></br></br></br></br></br>
-
-                    <h3>JIANT WEB AGENCY BASED IN ROMA, ITALIA </br></br> WEB DEVELOPERS - SEO - MARKETING - SOCIAL - GRAPHICS</h3>
+                    <img src="{{ asset('images/logos/jiant3.png') }}" style="height: 8vw; z-index:4;"/>
+                    </br>
+                    <h3>JIANT WEB AGENCY BASED IN ROMA, ITALIA </br></br> WEB DEVELOPERS | SEO | MARKETING | SOCIAL | GRAPHICS</h3>
 
                     </br></br></br></br></br></br></br>
 
-                    <div style="background-color:#000000d9; color:white; display:flex; width:100vw; flex-direction:row; flex-wrap:wrap; justify-content:center;">
+                    <div style="color:white; display:flex; width:100vw; flex-direction:row; flex-wrap:wrap; justify-content:center;">
+
+                        </br></br>
 
                         <div class="home_page_box">
                             <h1>CORPORATE IDENTITY & GRAFICA</h1>
@@ -81,7 +89,7 @@
                             </h6>
                             <i class="material-icons-round box_icon">rocket_launch</i>
                         </div>
-
+                        </br></br>
                         <div class="home_page_box">
                             <h1>COPYWRITING</h1>
                             <h6 class="home_page_box_spiega">
@@ -90,7 +98,7 @@
                             </h6>
                             <i class="material-icons-round box_icon">history_edu</i>
                         </div>
-
+                        </br></br>
                         <div class="home_page_box">
                             <h1>SERVIZI FOTOGRAFICI</h1>
                             <h6 class="home_page_box_spiega">
@@ -98,7 +106,7 @@
                             </h6>
                             <i class="material-icons-round box_icon">camera</i>
                         </div>
-
+                        </br></br>
                         <div class="home_page_box">
                             <h1>SITI E APPLICATIVI WEB E MOBILE</h1>
                             <h6 class="home_page_box_spiega">
@@ -107,7 +115,7 @@
                             </h6>
                             <i class="material-icons-round box_icon">devices</i>
                         </div>
-
+                        </br></br>
                         <div class="home_page_box">
                             <h1>SEO</h1>
                             <h6 class="home_page_box_spiega">
@@ -115,7 +123,18 @@
                             </h6>
                             <i class="material-icons-round box_icon">trending_up</i>
                         </div>
+                        </br></br>
+                        <div class="home_page_box">
+                            <h1>UX / UI</h1>
+                            <h6 class="home_page_box_spiega">
+                                User Experience / User interface.
+                            </h6>
+                            <i class="material-icons-round box_icon">extension</i>
+                        </div>
 
+
+
+                        </br></br>
                         <div class="home_page_box">
                             <h1>SOCIAL MEDIA MARKETING</h1>
                             <h6 class="home_page_box_spiega">
@@ -123,36 +142,155 @@
                             </h6>
                             <i class="material-icons-round box_icon">bar_chart</i>
                         </div>
-
+                        </br></br>
                         <div class="home_page_box">
                             <h1>CONSULENZA</h1>
                             <h6 class="home_page_box_spiega">
                                 Se vuoi avere una visione dello stato attuale della tua situazione online, possiamo offrire una analisi approfondita che ti permetterà di ottimizzare le aree che necessitano di intervento e ti proponiamo soluzioni.
                             </h6>
                             <i class="material-icons-round box_icon">handshake</i>
+                            </br></br></br></br>
+                        </div>
+
+                        </br></br>
+
+
+
+                        <div style="display: flex;width: 100vw;justify-content: center;align-content: center;align-items: center; background-color:white; color:black;">
+
+                            <div id="mainWrapper">
+                                <div id="cardsWrapper">
+
+                                    <div class="card">
+                                        <div class="image first" style="background-image: url({{ asset('images/bg/jup1.png') }});">
+                                            <div class="screen"></div>
+                                            <div class="text">
+                                                <img src="{{ asset('images/logos/jiant2.png') }}" style=" height: 4vw;"/>
+                                                <h1>Jupiter</h1>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="image first" style="background-image: url({{ asset('images/bg/jup2.jpeg') }});">
+                                            <div class="screen"></div>
+                                            <div class="text">
+                                                <img src="{{ asset('images/logos/jiant3.png') }}" style=" height: 4vw;"/>
+                                                <h1>Jupiter</h1>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="image first" style="background-image: url({{ asset('images/bg/jup3.jpeg') }});">
+                                            <div class="screen"></div>
+                                            <div class="text">
+                                            <img src="{{ asset('images/logos/jiant2.png') }}" style=" height: 4vw;"/>
+                                                <h1>Jupiter</h1>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    {{--<div class="card">
+                                        <div class="image first" style="background-image: url({{ asset('images/bg/jup3.jpeg') }});">
+                                            <div class="screen"></div>
+                                            <div class="text">
+                                            <img src="{{ asset('images/logos/jiant2.png') }}" style=" height: 4vw;"/>
+                                                <h1>Jupiter</h1>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="image first" style="background-image: url({{ asset('images/bg/jup2.jpeg') }});">
+                                            <div class="screen"></div>
+                                            <div class="text">
+                                            <img src="{{ asset('images/logos/jiant2.png') }}" style=" height: 4vw;"/>
+                                                <h1>Jupiter</h1>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="card">
+                                        <div class="image first" style="background-image: url({{ asset('images/bg/jup1.png') }});">
+                                            <div class="screen"></div>
+                                            <div class="text">
+                                            <img src="{{ asset('images/logos/jiant2.png') }}" style=" height: 4vw;"/>
+                                                <h1>Jupiter</h1>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>--}}
+
+                                </div>
+                            </div>
                         </div>
 
 
 
+
+                        <div style="display: flex;height: 100vh;width: 100vw;justify-content: center;align-content: center;align-items: center;">
+
+                        <div>
+                            <h3 style="width:90vw;">
+                                At JIANT we only want to build your websites and apps! We want to enjoy working with you on your projects, and we want you to enjoy our end results!
+                                </br></br></br></br>
+                                If youre interested in having us onboard..
+                                </br></br></br></br>
+                                If you want to take your activity online..
+                                </br></br></br></br>
+                                If you would like to start a new online project..
+                                </br></br></br></br>
+                                Send an email immediately to begin talking about what your next step should be!
+                            </h3>
+                        </div>
+
+                        </div>
+
                     </div>
+
+
+
+
+
 
                     <div style="background-color:black; color:white; display:flex; width:100vw; flex-direction:row; flex-wrap:wrap; justify-content:center;">
                         <div>
                             </br>
-                            <div>
-
-                                CONTATTACI
-                                JIANT
-
-                                Via Libya, 3
-                                00195 Roma (RM) – Italia
-
-                                Scrivici
-                                leyton@jiant.com
-
-                                Siamo lavorando
-                                7 giorni al settimana
-
+                            <img src="{{ asset('images/logos/jiant3.png') }}" style="height: 8vw; z-index:4;"/>
+                            </br>
+                            <h1>CONTATTACI</h1>
+                            <div style="display: flex;justify-content: space-evenly;width: 100vw;">
+                                <div>
+                                    <h5>
+                                        JIANT
+                                        Via Libia
+                                        00199 Roma, RM, Italia
+                                    </h5>
+                                </div>
+                                <div>
+                                    <h5>
+                                        Scrivici
+                                        team@jiant.io
+                                    </h5>
+                                </div>
+                                <div>
+                                    <h5>
+                                        Whatsapp
+                                        +39 375 5499384
+                                    </h5>
+                                </div>
+                                <div>
+                                    <h5>
+                                        Siamo lavorando
+                                        7 giorni al settimana
+                                    </h5>
+                                </div>
 
                             </div>
                             </br>
@@ -171,6 +309,8 @@
                             </div>
                         </div>
                     </div>
+
+
 
                     </br>
 
